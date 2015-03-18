250212133b3c1c555d89edd4b708fa14ad22285b
Riuchando <sdkinser456@gmail.com>
2015-03-18 06:51:57 -0500

    3-18-2015

---
+++ hartisabuttocks/test/hartisabuttocks/core_test.clj
@@ -0,0 +1,7 @@
+(ns hartisabuttocks.core-test
+  (:require [clojure.test :refer :all]
+            [hartisabuttocks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
