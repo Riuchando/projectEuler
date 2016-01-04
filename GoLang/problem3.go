5bb733a28a0b25464d47334bb64cc737a8e76764
Riuchando <sdkinser456@gmail.com>
2016-01-04 17:57:31 -0600

    Golang

---
+++ GoLang/problem3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
+func main() {
+
+	var number int64 =600851475143
+	var i int64=2
+	for i < number {
+	if number%i==0{
+	number= number/i
+	}
+	i++
+	}
+	fmt.Println("Largest Prime Factor:",i)
+}
